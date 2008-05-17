ecdf54935bb6657cf9f6194218f556baa549d549
mh <mh@immerda.ch>
2008-05-17 10:10:34 +0000

    fixed language typo

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -145,7 +145,7 @@ define djbdns::addCname(
 
 define djbdns::managed_file () {
     concatenated_file { "/var/lib/puppet/modules/djbdns/$name":
-        $dir => "/var/lib/puppet/modules/djbdns/${name}.d",
+        dir => "/var/lib/puppet/modules/djbdns/${name}.d",
         owner => root, group => 0, mode => 0600;
     }
 }
