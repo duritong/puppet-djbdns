816807d52b985d368930a9349275f72e0892a2ed
mh <mh@immerda.ch>
2008-07-04 15:23:33 +0000

    renamed Reverse to pointer and handle it really only as a pointer

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -159,7 +159,7 @@ define djbdns::addSPF(
     }
 }
 
-define djbdns::addReverse(
+define djbdns::addPointer(
     $ip,
     $domain = 'absent',
     $ttl = '3600',
@@ -170,7 +170,7 @@ define djbdns::addReverse(
         default => $domain
     }
     djbdns::entry{"reverse.d/000-reverse-${name}":
-        line => "=${real_domain}:${ip}:${ttl}::${location}",
+        line => "^${real_domain}:${ip}:${ttl}::${location}",
     }
 }
 
