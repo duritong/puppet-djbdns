66d554d072c72096cbeb060b23b1f21ca658e011
mh <mh@immerda.ch>
2008-07-04 15:23:38 +0000

    added rpm infos to find packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
 # modules/djbdns/manifests/init.pp - manage djbdns stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
+# For rpms for redhat, centos etc. you might want to look here:
+# http://summersoft.fay.ar.us/pub/qmail/djbdns/
+#
 
 modules_dir { "djbdns": }
 
