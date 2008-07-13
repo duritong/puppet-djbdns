cffee83af2b4b2740ed0858dd1dfa689062b7fbd
mh <mh@immerda.ch>
2008-07-13 13:11:03 +0000

    improved mailserver function

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -106,6 +106,11 @@ define djbdns::addnameserver(
     }
 }
 
+# if you set the mailserverip
+# to the default, it will add 
+# a A name as well
+# if you point it to something
+# else, it have to be domain
 define djbdns::addmailserver(
     $domain = '',
     $mailserverip = '212.103.72.240',
@@ -116,12 +121,21 @@ define djbdns::addmailserver(
         '' => $name,
         default => $domain
     }
-    djbdns::entry{"mx-records.d/000-mx-records-${name}":
-        line => "@${real_domain}::mail.${real_domain}.:${priority}:${ttl}::",
-    }
-    djbdns::addArecord{"mailserver-${name}":
-        a_record => "mail.${real_domain}",
-        ip => $mailserverip,
+    case $mailserverip {
+        '212.103.72.240': {
+            djbdns::entry{"mx-records.d/000-mx-records-${name}":
+                line => "@${real_domain}::mail.${real_domain}.:${priority}:${ttl}::",
+            }
+            djbdns::addArecord{"mailserver-${name}":
+                a_record => "mail.${real_domain}",
+                ip => $mailserverip,
+            }
+        }
+        default: {
+            djbdns::entry{"mx-records.d/000-mx-records-${name}":
+                line => "@${real_domain}::${mailserverip}.:${priority}:${ttl}::",
+            }
+        }
     }
 }
 
