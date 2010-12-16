00e8ea0199b30667749040750853fd677eee0327
mh <mh@immerda.ch>
2010-12-16 13:21:39 +0100

    manage dnslog user on centos as package doesn't create it

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,5 +1,13 @@
 class djbdns::centos inherits djbdns::base {
 
+  user::managed{ "dnslog":
+    homedir => "/nonexistent",
+    managehome => false,
+    shell => "/usr/sbin/nologin",
+    before => Package['djbdns'],
+    uid => 103, gid => 103;
+  }
+
   service{'djbdns':
     ensure => running,
     enable => true,
