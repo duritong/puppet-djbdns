28fe4c4ab54eec6645bfa466c2618c1ee4751a36
mh <mh@immerda.ch>
2010-12-16 13:17:44 +0100

    intendation

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,17 +1,18 @@
 class djbdns::centos inherits djbdns::base {
-    service{'djbdns':
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package['djbdns'],
-    }
 
-    file{'/etc/sysconfig/djbdns.secondaries':
-        source => [ "puppet:///modules/site-djbdns/sysconfig/${fqdn}/djbdns.secondaries",
-                    "puppet:///modules/site-djbdns/sysconfig/djbdns.secondaries",
-                    "puppet:///modules/djbdns/sysconfig/${operatingsystem}/djbdns.secondaries",
-                    "puppet:///modules/djbdns/sysconfig/djbdns.secondaries" ],
-        require => Package['djbdns'],
-        owner => root, group => 0, mode => 0644;
-    }
+  service{'djbdns':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package['djbdns'],
+  }
+
+  file{'/etc/sysconfig/djbdns.secondaries':
+    source => [ "puppet:///modules/site-djbdns/sysconfig/${fqdn}/djbdns.secondaries",
+                "puppet:///modules/site-djbdns/sysconfig/djbdns.secondaries",
+                "puppet:///modules/djbdns/sysconfig/${operatingsystem}/djbdns.secondaries",
+                "puppet:///modules/djbdns/sysconfig/djbdns.secondaries" ],
+    require => Package['djbdns'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
